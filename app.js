5694cc94d78d2449aada2de9bdc7fa0026df94b5
Alan Hou <houych@gmail.com>
2024-10-17 21:06:44 -0700

    save contents to a bucket

--- app.js
+++ app.js
@@ -1,32 +1,33 @@
-// Copyright 2021 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+import express from 'express'
+import { pinoHttp, logger } from './utils/logging.js'
+import { Storage } from '@google-cloud/storage'
+import dotenv from 'dotenv'
+dotenv.config()
 
-import express from 'express';
-import {pinoHttp, logger} from './utils/logging.js';
-
-const app = express();
+const app = express()
+// Middleware to parse JSON request bodies
+app.use(express.json())
 
 // Use request-based logger for log correlation
-app.use(pinoHttp);
+app.use(pinoHttp)
 
 // Example endpoint
-app.get('/', async (req, res) => {
+app.post('/', async (req, res) => {
   // Use basic logger without HTTP request info
-  logger.info({logField: 'custom-entry', arbitraryField: 'custom-entry'}); // Example of structured logging
+  logger.info({ logField: 'custom-entry', arbitraryField: 'custom-entry' }) // Example of structured logging
   // Use request-based logger with log correlation
-  req.log.info('Child logger with trace Id.'); // https://cloud.google.com/run/docs/logging#correlate-logs
-  res.send('Hello World!');
-});
+  req.log.info('Child logger with trace Id.') // https://cloud.google.com/run/docs/logging#correlate-logs
+  const { contents } = req.body
 
-export default app;
+  if (!contents) {
+    return res.status(400).send('contents is required')
+  }
+  const storage = new Storage()
+  await storage
+    .bucket(process.env.BUCKET_NAME || '')
+    .file(process.env.OBJECT_NAME || '')
+    .save(contents)
+  res.send('contents saved')
+})
+
+export default app
