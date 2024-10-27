dcbf2f2aa6ae8fce122763206a8570fd7ee5bdd8
Alan Hou <houych@gmail.com>
2024-10-26 18:36:47 -0700

    add get transcription endpoint with callSid as a param

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { pinoHttp, logger } from './utils/logging.js'
+import { pinoHttp } from './utils/logging.js'
 import { Storage } from '@google-cloud/storage'
 import dotenv from 'dotenv'
 dotenv.config()
@@ -11,28 +11,43 @@ app.use(express.json())
 // Use request-based logger for log correlation
 app.use(pinoHttp)
 
-// Example endpoint
-app.put('/', async (req, res) => {
-  // Use basic logger without HTTP request info
-  logger.info({ logField: 'custom-entry', arbitraryField: 'custom-entry' }) // Example of structured logging
-  // Use request-based logger with log correlation
-  req.log.info('Child logger with trace Id.') // https://cloud.google.com/run/docs/logging#correlate-logs
-  const { contents } = req.body
-
-  if (!contents) {
-    return res.status(400).send('contents is required')
-  }
+function getBucket() {
   const storage = new Storage()
   const bucketName = process.env.BUCKET_NAME
   if (!bucketName) {
-    return res.status(500).send('unable to find bucket name')
+    throw new Error('unable to find bucket name')
+  }
+  return storage.bucket(bucketName)
+}
+
+app.put('/', async (req, res) => {
+  req.log.info({ logField: 'custom-entry' }) // https://cloud.google.com/run/docs/logging#correlate-logs
+  const { contents } = req.body
+  if (!contents) {
+    return res.status(400).send('contents is required')
   }
   const fileName = process.env.OBJECT_NAME
-  if (!bucketName) {
-    return res.status(500).send('unable to find object name')
-  }
-  await storage.bucket(bucketName).file(fileName).save(contents)
+  await getBucket().file(fileName).save(contents)
   res.send({ fileName: fileName })
+})
+
+app.get('/transcription/:callSid', async (req, res) => {
+  const { callSid } = req.params
+  if (!callSid) {
+    return res.status(400).send('callSid is required')
+  }
+  req.log.info({ callSid })
+  const transcriptionDirectory =
+    process.env.GCP_STORAGE_TRANSCRIPTIONS_DIRECTORY
+  if (!transcriptionDirectory) {
+    return res.status(500).send('unable to find transcription directory')
+  }
+  const fileName = `${transcriptionDirectory}${callSid}.json`
+  const fileContent = await getBucket().file(fileName).download()
+  const transcription = JSON.parse(fileContent.toString()).filter(
+    (message) => message.role !== 'system',
+  )
+  res.send(transcription)
 })
 
 export default app
